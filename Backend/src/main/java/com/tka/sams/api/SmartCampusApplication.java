d05d24f706093a6804c4b3df1a2884d594eb5556
Mayurmali2611 <malimayur477@gmail.com>
2025-09-21 14:26:07 +0530

    added Files

---
+++ Backend/src/main/java/com/tka/sams/api/SmartCampusApplication.java
@@ -0,0 +1,14 @@
+package com.tka.sams.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCampusApplication.class, args);
+		System.out.println("Application Is Started..");
+	}
+
+}
